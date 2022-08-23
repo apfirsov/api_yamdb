42d1b8fe33440edc8431466a34abd01646b29a60
Александр Смирнов <5targazer@mail.ru>
2022-08-23 22:44:18 +0300

    fixed User model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -2,19 +2,23 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-ROLES = [
-    ('user', 'пользователь'),
-    ('moderator', 'модератор'),
-    ('admin', 'администратор')
-]
-
-
 class User(AbstractUser):
+
+    class UserRoles(models.TextChoices):
+        USER = 'USR', 'Пользователь'
+        MODERATOR = 'MOD', 'Модератор'
+        ADMIN = 'ADM', 'Администратор'
+
+    email = models.EmailField(
+        'Адрес электронной почты',
+        max_length=254,
+        unique=True
+    )
     role = models.CharField(
         'Роль',
-        max_length=50,
-        choices=ROLES,
-        default='user'
+        max_length=3,
+        choices=UserRoles.choices,
+        default=UserRoles.USER
     )
     bio = models.TextField(
         'Биография',
