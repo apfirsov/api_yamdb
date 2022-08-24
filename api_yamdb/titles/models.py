8df0aa3a60f0a780d8d6bf066c8cb9d23bbb56fb
Антон Панов <ber42@yandex.ru>
2022-08-24 23:36:54 +0300

    Комит с исправленными моделями

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Genres(models.Model):
+class Genre(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(unique=True)
 
@@ -9,7 +9,7 @@ class Genres(models.Model):
         return self.name
 
 
-class Categories(models.Model):
+class Categorie(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(unique=True, max_length=50)
 
@@ -17,18 +17,18 @@ class Categories(models.Model):
         return self.name
 
 
-class Titles(models.Model):
+class Title(models.Model):
     name = models.CharField()
     year = models.IntegerField(max_length=4)
-    description = models.TextField(blank=True)
+    description = models.TextField(null=True, blank=True)
     genre = models.ManyToManyField(
-        Genres,
+        Genre,
         through='GenresTitles',
         on_delete=models.SET_NULL,
         null=True
     )
     category = models.ForeignKey(
-        Categories,
+        Categorie,
         related_name='titles',
         on_delete=models.SET_NULL,
         null=True
@@ -38,9 +38,9 @@ class Titles(models.Model):
         return self.name
 
 
-class GenresTitles(models.Model):
-    genre = models.ForeignKey(Genres, on_delete=models.CASCADE)
-    title = models.ForeignKey(Titles, on_delete=models.CASCADE)
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.genre} {self.title}'
