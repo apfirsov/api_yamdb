7bace72868aab963f6dd86e72e83e8cbfaf7198c
Антон Панов <ber42@yandex.ru>
2022-08-24 19:46:10 +0300

    Исправил модели

---
+++ api_yamdb/titles/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    name = 'titles'
+    verbose_name = 'Произведения'
--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -18,20 +18,29 @@ class Categories(models.Model):
 
 
 class Titles(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField()
     year = models.IntegerField(max_length=4)
-    description = models.TextField()
-    genre = models.ManyToManyField(Genres, through='GenresTitles')
-    category = models.ForeignKey(Categories, related_name='titles')
+    description = models.TextField(blank=True)
+    genre = models.ManyToManyField(
+        Genres,
+        through='GenresTitles',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    category = models.ForeignKey(
+        Categories,
+        related_name='titles',
+        on_delete=models.SET_NULL,
+        null=True
+    )
 
     def __str__(self):
         return self.name
 
 
 class GenresTitles(models.Model):
-    genre = models.ForeignKey(Genres)
-    title = models.ForeignKey(Titles)
+    genre = models.ForeignKey(Genres, on_delete=models.CASCADE)
+    title = models.ForeignKey(Titles, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.genre} {self.title}'
-# Реализовать удаление всего при удалении Тайтла
