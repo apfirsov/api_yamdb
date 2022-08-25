63f161c8e53636e6349c3883ea0d7ef99c0fc5b9
Artem Firsov <firsov.ip@gmail.com>
2022-08-26 00:42:52 +0300

    Revert "Merge branch 'feature/jwt' of github.com:apfirsov/api_yamdb into feature/reviews.api"
    This reverts commit 6f939946cd13e41864974e425798a4cafb774a0f, reversing
    changes made to 4a77f584392278e3c97baff271b85155f579e1b3.
    
    	modified:   api_yamdb/api_yamdb/settings.py
    	modified:   api_yamdb/api_yamdb/urls.py
    	deleted:    api_yamdb/users/serializers.py
    	modified:   api_yamdb/users/urls.py
    	deleted:    api_yamdb/users/views.py
    	modified:   requirements.txt

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,21 +1,10 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .views import sign_up_view, MyTokenObtainSlidingView, UserViewSet
-
-
-router = routers.DefaultRouter()
-router.register('', UserViewSet)
 
 urlpatterns = [
     path(
-        'signup/',
-        sign_up_view,
-        name='signup'),
-    path(
-        'token/',
-        MyTokenObtainSlidingView.as_view(),
+        'auth/signup/',
+        TokenObtainPairView.as_view(),
         name='token_obtain_pair'
     ),
-    path('', include(router.urls))
 ]
