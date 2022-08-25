435b8f6563ead80a65ae4b6729ec963990651952
Александр Смирнов <5targazer@mail.ru>
2022-08-25 18:35:50 +0300

    a lot of stuff done

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,10 +1,21 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .views import sign_up_view, MyTokenObtainSlidingView, UserViewSet
+
+
+router = routers.DefaultRouter()
+router.register('', UserViewSet)
 
 urlpatterns = [
     path(
-        'auth/signup/',
-        TokenObtainPairView.as_view(),
+        'signup/',
+        sign_up_view,
+        name='signup'),
+    path(
+        'token/',
+        MyTokenObtainSlidingView.as_view(),
         name='token_obtain_pair'
     ),
+    path('', include(router.urls))
 ]
