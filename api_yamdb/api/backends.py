eac8fe09928605afc3989ac52fe3f2587d96a5b1
Александр Смирнов <5targazer@mail.ru>
2022-08-29 21:34:52 +0300

    fixed AUTHENTICATION_BACKENDS

--- api_yamdb/api/backends.py
+++ api_yamdb/api/backends.py
@@ -7,7 +7,6 @@ class AuthBackend(ModelBackend):
 
     def authenticate(self, request, username=None, confirmation_code=None):
         user = User.objects.get(username=username)
-        print(confirmation_code)
         if default_token_generator.check_token(user, confirmation_code):
             return user
         return None
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -126,5 +126,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
 AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
     'api.backends.AuthBackend',
 ]
