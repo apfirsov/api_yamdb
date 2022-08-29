edf107fd167937f69d35b6da4e5b170669a8c3da
Александр Смирнов <5targazer@mail.ru>
2022-08-29 23:36:47 +0300

    fixed AuthBackend: added try-except

--- api_yamdb/api/backends.py
+++ api_yamdb/api/backends.py
@@ -6,7 +6,10 @@ from users.models import User
 class AuthBackend(ModelBackend):
 
     def authenticate(self, request, username=None, confirmation_code=None):
-        user = User.objects.get(username=username)
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return None
         if default_token_generator.check_token(user, confirmation_code):
             return user
         return None
