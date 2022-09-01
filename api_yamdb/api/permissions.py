8a6787c670ff964f70dac510666187cb29c2e494
Александр Смирнов <5targazer@mail.ru>
2022-09-01 22:14:53 +0300

    fixed AuthorOrStaffOrReadOnly permission

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -47,5 +47,5 @@ class AuthorOrStaffOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in SAFE_METHODS
                 or (obj.author == request.user
-                    or request.user.is_superuser
-                    or request.user.role in [User.ADMIN, User.MODERATOR]))
+                    or request.user.is_admin
+                    or request.user.role == User.MODERATOR))
