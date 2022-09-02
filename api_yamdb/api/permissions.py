42bc7b96d49beeec080bc53075bc88dd55ebef0a
Александр Смирнов <5targazer@mail.ru>
2022-09-02 05:35:24 +0300

    refactored permissions syntax

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,24 +7,17 @@ class IsAdmin(BasePermission):
         return (request.user.is_authenticated
                 and request.user.is_admin)
 
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_admin
-
 
 class IsAdminOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-
-        return (request.user.is_authenticated
-                and request.user.is_admin)
+        return (request.method in SAFE_METHODS
+                or (request.user.is_authenticated
+                    and request.user.is_admin))
 
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-
-        return request.user.is_admin
+        return (request.method in SAFE_METHODS
+                or request.user.is_admin)
 
 
 class IsUserOrAdmin(BasePermission):
@@ -45,5 +38,5 @@ class AuthorOrStaffOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (request.method in SAFE_METHODS
-                or (obj.author == request.user
-                    or request.user.is_moderator))
+                or obj.author == request.user
+                or request.user.is_moderator)
