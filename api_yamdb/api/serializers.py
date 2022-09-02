08e0bedbee7d09e01ab90eea22169b84cf3a7e3f
Александр Смирнов <5targazer@mail.ru>
2022-09-02 03:39:47 +0300

    TokenSerializer heavily simplified

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -15,49 +15,22 @@ class TokenSerializer(serializers.Serializer):
 
     username_field = User.USERNAME_FIELD
 
-    default_error_messages = {
-        'no_active_account': ('No active account found '
-                              'with the given credentials')
-    }
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.fields[self.username_field] = serializers.CharField()
         self.fields['confirmation_code'] = serializers.CharField()
 
-    @classmethod
-    def get_token(cls, user):
-        return RefreshToken.for_user(user)
-
     def validate(self, attrs):
         authenticate_kwargs = {
             self.username_field: attrs[self.username_field],
             'confirmation_code': attrs['confirmation_code'],
         }
-        try:
-            authenticate_kwargs['request'] = self.context['request']
-        except KeyError:
-            pass
 
         self.user = authenticate(**authenticate_kwargs)
 
         if not api_settings.USER_AUTHENTICATION_RULE(self.user):
-            raise exceptions.ParseError(
-                self.error_messages['no_active_account'],
-                'no_active_account',
-            )
-
-        refresh = self.get_token(self.user)
-
-        data = {}
-        data['refresh'] = str(refresh)
-        data['access'] = str(refresh.access_token)
-
-        if api_settings.UPDATE_LAST_LOGIN:
-            update_last_login(None, self.user)
-
-        return data
+            raise exceptions.ParseError('No active account found')
 
 
 class SignUpSerializer(serializers.ModelSerializer):
